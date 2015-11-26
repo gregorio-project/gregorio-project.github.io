3b3be9973a4c69065e24d753596f09ebce3f5d87
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-11-26 17:28:00 -0500

    Automating image conversion

--- tutorial/process.sh
+++ tutorial/process.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 lualatex --shell-escape ${1}-main.tex
-okular ${1}-main.pdf
-rm -f $1.gtex ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log ${1}-main.synctex.gz
+convert -density 400 ${1}-main.pdf -trim -quality 100 -resize 640 $1.png
+rm -f $1-*.gtex ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log ${1}-main.synctex.gz
 
