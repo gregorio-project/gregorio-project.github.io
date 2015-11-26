a74aa5a46fafc6cf9227c21ee7457f5f4956d043
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-11-26 16:27:34 -0500

    Updating scripts that aid with generating diagrams

--- tutorial/process.sh
+++ tutorial/process.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-gregorio $1.gabc
-lualatex ${1}-main.tex
+lualatex --shell-escape ${1}-main.tex
 okular ${1}-main.pdf
-rm -f $1.tex ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log
+rm -f $1.gtex ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log ${1}-main.synctex.gz
 
