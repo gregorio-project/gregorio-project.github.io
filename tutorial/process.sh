612d35d72c443c7b207725cf84c6d93507c44947
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-11-27 16:17:23 -0500

    White centers weren't being totally removed

--- tutorial/process.sh
+++ tutorial/process.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 lualatex --shell-escape ${1}-main.tex
-convert -density 174 ${1}-main.pdf -trim -quality 100 -transparent white $1.png
+convert -density 174 ${1}-main.pdf -trim -quality 100 -fuzz 10% -transparent white $1.png
 rm -f $1-*.gtex ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log ${1}-main.synctex.gz
 
