dfcc6e41cd216523eda2865bcbe9c0e371cb1cb6
Br. Samuel Springuel <rpspringuel@gmail.com>
2015-11-27 15:55:29 -0500

    Make image size dependent on resolution and not fixed
    By tuning the resolution down to just what is needed to get the size images I want in the tutorial, I can eliminate the resize argument, allowing the images created to be just as big as they need to be.  This allows the script to generate some of the smaller images.

--- tutorial/process.sh
+++ tutorial/process.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 lualatex --shell-escape ${1}-main.tex
-convert -density 400 ${1}-main.pdf -trim -quality 100 -resize 640 -transparent white $1.png
+convert -density 174 ${1}-main.pdf -trim -quality 100 -transparent white $1.png
 rm -f $1-*.gtex ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log ${1}-main.synctex.gz
 
