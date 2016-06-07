a5cba5ba8089e9d796f9aa3a2bb9f827044bd1c5
Br. Samuel Springuel <rpspringuel@gmail.com>
2016-06-07 11:57:23 -0400

    Remove glog files on completion.

--- tutorial/process.sh
+++ tutorial/process.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 latexmk -e 'push @generated_exts, "gaux";' -pdf -pdflatex='lualatex --shell-escape' ${1}-main.tex &&
 convert -density 174 ${1}-main.pdf -trim -quality 100 -fuzz 10% -transparent white $1.png &&
-rm -f $1-*.gtex ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log ${1}-main.synctex.gz ${1}-main.fls ${1}-main.fdb_latexmk
+rm -f $1-*.gtex $1-*.glog ${1}-main.pdf ${1}-main.aux ${1}-main.gaux ${1}-main.log ${1}-main.synctex.gz ${1}-main.fls ${1}-main.fdb_latexmk
 
